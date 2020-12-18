2a5d10059982083230450851a913ccf5a694579e
nemetz07 <nemetz07@gmail.com>
2020-12-18 21:03:00 +0100

    setting up routes/alarm

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <controller/HomeController.h>
+#include <SPIFFS.h>
+#include <controller/SettingsController.h>
+#include <TimeAlarms.h>
+#include <controller/ScheduleController.h>
 #include "TimeUtil.h"
-#include "Motor.h"
 
-Motor motor = Motor();
+#define SERVER_PORT 3000
 
-void setup() {
-    Serial.begin(115200);
-    delay(250);
+AsyncWebServer server(SERVER_PORT);
 
-    Serial.println("TimeNTP Example");
+void setupSPIFFS() {
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+}
+
+void setupWifi() {
     Serial.print("Connecting to ");
     Serial.println(WIFI_SSID);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
@@ -21,22 +31,55 @@ void setup() {
 
     Serial.print("IP number assigned by DHCP is ");
     Serial.println(WiFi.localIP());
-    Serial.println("Starting UDP");
+}
+
+void setupTimeSync() {
     TimeUtil::init();
     Serial.println("Current time: ");
     TimeUtil::digitalClockDisplay();
+}
 
-    MotorAction action1(0,90,1000);
-    MotorAction action2(90,180,1000);
-    MotorAction action3(180,90,1000);
-    MotorAction action4(90,0,1000);
+void setupRoutes() {
+    //Static files
+    server.serveStatic("/sb-admin.css", SPIFFS, "/sb-admin.css");
+    server.serveStatic("/bootstrap.min.css", SPIFFS, "/bootstrap.min.css");
+    server.serveStatic("/bootstrap.min.js", SPIFFS, "/bootstrap.min.js");
+    server.serveStatic("/sb-admin.js", SPIFFS, "/sb-admin.js");
+    server.serveStatic("/jquery-3.5.1.min.js", SPIFFS, "/jquery-3.5.1.min.js");
 
-    motor.addAction(action1);
-    motor.addAction(action2);
-    motor.addAction(action3);
-    motor.addAction(action4);
+    //View routes
+    server.on("/", HTTP_GET, HomeController::index);
+    server.on("/settings", HTTP_GET, SettingsController::index);
+
+    //API routes
+    server.on("/api/servoPin", HTTP_POST, SettingsController::setServoPin);
+    server.on("/api/testMotor", HTTP_POST, SettingsController::testMotor);
+    server.on("/api/alarm", HTTP_POST, ScheduleController::addAlarm);
+
+}
+
+void setupServer() {
+    Serial.println("Setting up server");
+    setupRoutes();
+
+    server.begin();
+
+    Serial.print("Started server at: ");
+    Serial.print(WiFi.localIP());
+    Serial.print(":");
+    Serial.println(SERVER_PORT);
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(250);
+
+    setupSPIFFS();
+    setupWifi();
+    setupTimeSync();
+    setupServer();
 }
 
 void loop() {
-    motor.start();
+    Alarm.delay(1000);
 }
